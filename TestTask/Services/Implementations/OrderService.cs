c18a1f8c3605f120d4e06b34d16010b5e63d8b85
E <gbvu2004@gmail.com>
2024-11-15 02:33:10 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public OrderService(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Order> GetOrder()
+        {
+            return await _dbContext.Orders
+                .Where(o => o.Quantity > 1)
+                .OrderByDescending(o => o.CreatedAt)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _dbContext.Orders.Where(o => o.User.Status == 0).OrderBy(o  => o.CreatedAt).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<User> GetUser()
+        {
+            var userIdWithMaxTotal = await _dbContext.Orders
+                .Where(order => order.Status == OrderStatus.Delivered && order.CreatedAt.Year == 2003)
+                .GroupBy(order => order.UserId)
+                .Select(group => new
+                {
+                    UserId = group.Key,
+                    TotalDeliveredSum = group.Sum(order => order.Price * order.Quantity)
+                })
+                .OrderByDescending(g => g.TotalDeliveredSum)
+                .Select(g => g.UserId)
+                .FirstOrDefaultAsync();
+
+            return await _dbContext.Users.FindAsync(userIdWithMaxTotal);
+        }
+
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _dbContext.Users
+                           .Where(u => u.Orders
+                               .Any(o => (o.Status == Enums.OrderStatus.Paid && o.CreatedAt.Year == 2010)))
+                           .ToListAsync();
+        }
+    }
+}
